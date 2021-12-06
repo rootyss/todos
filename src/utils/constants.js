cad90a328a7e4c7bba65fda7d9fabac5ecde530d
rootyss <jaxent@yandex.ru>
2021-12-06 16:38:49 +0300

    add path constants

---
+++ src/utils/constants.js
@@ -0,0 +1,5 @@
+export const LOGIN_ROUTE = '/login';
+export const TODO_ROUTE = '/todo';
+export const REG_ROUTE = '/signup';
+export const NOTE_ROUTE = '/note';
+export const USER_PROFILE_ROUTE = '/note';
