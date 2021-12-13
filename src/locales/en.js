54aa16fee64ed78b7c22a566937df4eeaee310fc
rootyss <jaxent@yandex.ru>
2021-12-13 14:59:07 +0300

    add i18next (en, ru)

---
+++ src/locales/en.js
@@ -0,0 +1,49 @@
+export default {
+  translation: {
+    buttons: {
+      login: 'LogIn',
+      signup: 'SignIn and LogIn',
+      logOut: 'LogOut',
+    },
+    label: {
+      email: 'Email',
+      password: 'Password',
+      confirmpassword: 'Confirm password',
+    },
+    placeholders: {
+      email: 'Email',
+      password: 'Password',
+      confirmpassword: 'Confirm password',
+    },
+    errors: {
+      'auth/wrong-password': 'Wrong password',
+      'auth/email-already-in-use': 'Email already in use',
+      'auth/network-request-failed': 'Network request failed',
+      incorrectEmail: 'Invalid email address',
+      requiredEmail: 'Email - required field',
+      requiredPassword: 'Password - required field',
+      minPasswordLength: 'Min 6 characters',
+      passMatch: 'Passwords must match',
+    },
+    logo: {
+      text: 'Task manager',
+    },
+    mainMenu: {
+      note: 'Notes',
+      login: 'LogIn',
+      signup: 'SignUp',
+      userAnonim: 'Anonymous',
+    },
+    loginPage: {
+      heading: 'LogIn',
+      notReg: 'Not registered?',
+      urlToSignUp: 'SignUp',
+
+    },
+    signupPage: {
+      heading: 'SignUp',
+      alredyReg: 'Already registered?',
+      urlToLogin: 'LogIn',
+    },
+  },
+};
---
+++ src/locales/ru.js
@@ -0,0 +1,49 @@
+export default {
+  translation: {
+    buttons: {
+      login: 'Вход',
+      signup: 'Зарегистрироваться и войти',
+      logOut: 'Выход',
+    },
+    label: {
+      email: 'Почта',
+      password: 'Пароль',
+      confirmpassword: 'Подтверждение пароля',
+    },
+    placeholders: {
+      email: 'Почта',
+      password: 'Пароль',
+      confirmpassword: 'Подтверждение пароля',
+    },
+    errors: {
+      'auth/wrong-password': 'Некорректный пароль',
+      'auth/email-already-in-use': 'Адрес электронной почты уже используется',
+      'auth/network-request-failed': 'Превышено число запросов',
+      incorrectEmail: 'Некорректно введён адрес электронной почты',
+      requiredEmail: 'Почта - обязательное поле',
+      requiredPassword: 'Пароль - обязательное поле',
+      minPasswordLength: 'Минимум 6 символов',
+      passMatch: 'Пароли должны совпадать',
+    },
+    logo: {
+      text: 'Менеджер задач',
+    },
+    mainMenu: {
+      note: 'Заметки',
+      login: 'Вход',
+      signup: 'Зарегистрироваться',
+      userAnonim: 'Аноним',
+    },
+    loginPage: {
+      heading: 'Вход',
+      notReg: 'Не зарегистрированы?',
+      urlToSignUp: 'Регистрация',
+
+    },
+    signupPage: {
+      heading: 'Регистрация',
+      alredyReg: 'Уже зарегистрированы?',
+      urlToLogin: 'Войти',
+    },
+  },
+};
