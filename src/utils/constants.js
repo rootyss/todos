cfb0fa4e4d0ea34aa202cbef7fc8b0e5f2953819
rootyss <jaxent@yandex.ru>
2021-12-14 22:45:23 +0300

    add routes to INBOX, UPCOMING and TODAY

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,8 @@
 export const LOGIN_ROUTE = '/login';
 export const TODO_ROUTE = '/todo';
 export const REG_ROUTE = '/signup';
-export const NOTE_ROUTE = '/note';
-export const USER_PROFILE_ROUTE = '/note';
+export const NOTE_ROUTE = 'notes';
+export const USER_PROFILE_ROUTE = '/user';
+export const INBOX_ROUTE = 'inbox';
+export const TODAY_ROUTE = 'today';
+export const UPCOMING_ROUTE = 'upcoming';
