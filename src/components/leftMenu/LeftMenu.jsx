0938fad7f2d4fb1fe817cc4cd6343a07e098e706
rootyss <jaxent@yandex.ru>
2021-12-14 22:39:46 +0300

    add left menu component

---
+++ src/components/leftMenu/LeftMenu.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import { Link } from 'react-router-dom';
+import {
+  INBOX_ROUTE,
+  TODAY_ROUTE,
+  UPCOMING_ROUTE,
+  NOTE_ROUTE,
+} from '../../utils/constants.js';
+
+const LeftMenu = () => {
+  const { t } = useTranslation();
+  return (
+    <div className="left-menu left-menu-light">
+      <div className="left-menu-list">
+        <ul className="left-menu-top">
+          <li><Link className="left-menu-nav-link" to={INBOX_ROUTE}>{t('leftMenuTop.inbox')}</Link></li>
+          <li><Link className="left-menu-nav-link" to={TODAY_ROUTE}>{t('leftMenuTop.today')}</Link></li>
+          <li><Link className="left-menu-nav-link" to={UPCOMING_ROUTE}>{t('leftMenuTop.upcoming')}</Link></li>
+          <li><Link className="left-menu-nav-link" to={NOTE_ROUTE}>{t('leftMenuTop.notes')}</Link></li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default LeftMenu;
