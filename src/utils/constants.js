7b2e4883da4569b170e5778c75d89064381164b4
rootyss <jaxent@yandex.ru>
2021-12-20 19:20:52 +0300

    add URL_DATABASE FIREBASE_TASKS_ROUTE firebaseConfig

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -6,7 +6,18 @@ export const USER_PROFILE_ROUTE = '/user';
 export const INBOX_ROUTE = 'inbox';
 export const TODAY_ROUTE = 'today';
 export const UPCOMING_ROUTE = 'upcoming';
+export const URL_DATABASE = 'https://todos-c6f25-default-rtdb.asia-southeast1.firebasedatabase.app';
+export const FIREBASE_TASKS_ROUTE = 'tasks/';
 
 export const modalTypes = {
   fastAddTask: 'FastAddTask',
+};
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyD-qPr-mmCbzc6chebJwfXRchyVYeUUg_4",
+  authDomain: "todos-c6f25.firebaseapp.com",
+  projectId: "todos-c6f25",
+  storageBucket: "todos-c6f25.appspot.com",
+  messagingSenderId: "222266062244",
+  appId: "1:222266062244:web:0a5d68bdbd75e4043d3d31",
 };
