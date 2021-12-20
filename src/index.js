33d45489f2fb891df03fe14394dd44106f606bb5
rootyss <jaxent@yandex.ru>
2021-12-20 19:18:04 +0300

    delete tests firebase func

--- src/index.js
+++ src/index.js
@@ -1,20 +1,12 @@
 import { initializeApp } from "firebase/app";
 import ReactDOM from 'react-dom';
 import app from './init.jsx';
+import { firebaseConfig } from './utils/constants.js';
 
 const startApp = async () => {
-  const firebaseConfig = {
-    apiKey: "AIzaSyD-qPr-mmCbzc6chebJwfXRchyVYeUUg_4",
-    authDomain: "todos-c6f25.firebaseapp.com",
-    projectId: "todos-c6f25",
-    storageBucket: "todos-c6f25.appspot.com",
-    messagingSenderId: "222266062244",
-    appId: "1:222266062244:web:0a5d68bdbd75e4043d3d31",
-  };
+  const instanceApp = initializeApp(firebaseConfig);
 
-  initializeApp(firebaseConfig);
-
-  const virtualDom = await app();
+  const virtualDom = await app(instanceApp);
 
   ReactDOM.render(
     virtualDom,
