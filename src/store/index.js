73e570dd2154020dce7c61de6f105e61f8224811
rootyss <jaxent@yandex.ru>
2021-12-24 21:03:57 +0300

    add state fetch and labels

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,11 @@
 import { modalSliceReducer } from './modalSlice.js';
 import { tasksSliceReducer } from './tasksSlice.js';
+import { labelsSliceReducer } from './labelsSlice.js';
+import { fetchingStateReducer } from './fetchingState.js';
 
 export default {
   modal: modalSliceReducer,
   tasksInfo: tasksSliceReducer,
+  labelsInfo: labelsSliceReducer,
+  fetchingState: fetchingStateReducer,
 };
