96344cf4695950f81efcf73e39aec3cf285a49d9
rootyss <jaxent@yandex.ru>
2021-12-24 20:57:08 +0300

    add Label components

---
+++ src/components/labels/Labels.jsx
@@ -0,0 +1,79 @@
+import React, { useRef, useEffect } from 'react';
+import { useTranslation } from 'react-i18next';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import useAuth from '../../hooks/useAuth.jsx';
+import useApi from '../../hooks/useApi.jsx';
+import Spinner from '../spinner/Spinner.jsx';
+
+const Labels = () => {
+  const { t } = useTranslation();
+  const auth = useAuth();
+  const userUid = auth.getUserUid();
+  const api = useApi();
+  const textArea = useRef(null);
+
+  const formik = useFormik({
+    initialValues: {
+      labls: '',
+    },
+    validateOnChange: false,
+    validationSchema: Yup.object({
+      labls: Yup.string().trim().required(),
+    }),
+    onSubmit: async (values, { resetForm }) => {
+      const { labls } = values;
+      const labels = labls.replace(/\s+/g, '').split(',');
+      try {
+        labels.forEach((label) => {
+          api.addLabelToFirebase({ label: label.toLowerCase(), userUid });
+        });
+        resetForm();
+      } catch (err) {
+        console.log(err);
+      }
+    },
+  });
+
+  useEffect(() => {
+    textArea.current.focus();
+  }, []);
+
+  return (
+    <div className="main-content-wrapper">
+      <div className="content">
+        <div className="page">
+          <header className="view-header border-bottom">
+            <h1>{t('labelsPage.header')}</h1>
+          </header>
+          <div className="labels-list" />
+          <form>
+            <textarea
+              className="add-task-field"
+              type="text"
+              name="labls"
+              required
+              placeholder={t('placeholders.addLabels')}
+              onChange={formik.handleChange}
+              value={formik.values.labls}
+              ref={textArea}
+            />
+            <div className="form-fast-add-task-buttons">
+              <button
+                className="btn btn-primary"
+                type="submit"
+                variant="primary"
+                onClick={formik.handleSubmit}
+                disabled={formik.isSubmitting}
+              >
+                {formik.isSubmitting ? <Spinner /> : t('buttons.add')}
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Labels;
