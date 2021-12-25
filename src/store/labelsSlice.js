d482b750ce7aff8fb682660ee29f4f79e101787c
rootyss <jaxent@yandex.ru>
2021-12-25 22:01:06 +0300

    add slice for labels and tasks

--- src/store/labelsSlice.js
+++ src/store/labelsSlice.js
@@ -1,17 +1,25 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const labelsSlice = createSlice({
-  name: 'labels',
+  name: 'labelsInfo',
   initialState: {
     labels: [],
+    fetchingState: 'none',
   },
   reducers: {
     addLabelToState(state, action) {
       const { labels } = action.payload;
       return { labels };
     },
+    pending: (state) => ({ ...state, fetchingState: 'pending' }),
+    fulfilled: (state) => ({ ...state, fetchingState: 'finished' }),
+    rejected: (state) => ({ ...state, fetchingState: 'error' }),
   },
 });
 
-export const { addLabelToState } = labelsSlice.actions;
+export const getLabels = (state) => state.labelsInfo.labels;
+export const getFetchingStateTasks = (state) => state.labelsInfo.fetchingState;
+export const {
+  addLabelToState, pending, fulfilled, rejected,
+} = labelsSlice.actions;
 export const labelsSliceReducer = labelsSlice.reducer;
--- src/store/tasksSlice.js
+++ src/store/tasksSlice.js
@@ -6,6 +6,7 @@ const tasksSlice = createSlice({
   initialState: {
     tasks: [],
     currentTabDetailsName: 'sub-tasks',
+    fetchingState: 'none',
   },
   reducers: {
     addTaskToState(state, action) {
@@ -18,14 +19,20 @@ const tasksSlice = createSlice({
       const { name } = action.payload;
       return { ...state, currentTabDetailsName: name };
     },
+    pending: (state) => ({ ...state, fetchingState: 'pending' }),
+    fulfilled: (state) => ({ ...state, fetchingState: 'finished' }),
+    rejected: (state) => ({ ...state, fetchingState: 'error' }),
   },
 });
 
+export const getFetchingStateTasks = (state) => state.tasksInfo.fetchingState;
 export const getTasks = (state) => state.tasksInfo.tasks;
 export const getCurrentTask = (state) => {
   const { currentTaskId } = state.modal;
   return state.tasksInfo.tasks.find((task) => task.id === currentTaskId);
 };
 export const getCurrentTabDetailsName = (state) => state.tasksInfo.currentTabDetailsName;
-export const { addTaskToState, setCurrentTabDetailsName } = tasksSlice.actions;
+export const {
+  addTaskToState, setCurrentTabDetailsName, pending, fulfilled, rejected,
+} = tasksSlice.actions;
 export const tasksSliceReducer = tasksSlice.reducer;
