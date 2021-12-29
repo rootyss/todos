8c80cf49f5d138542b5c341b260b50809519a0d7
rootyss <jaxent@yandex.ru>
2021-12-29 18:09:51 +0300

    add cookie consent

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import { useCookies } from 'react-cookie';
+import CookieConsent from '../cookieConsent/CookieConsent.jsx';
 
-const Footer = () => (
-  <footer className="border-top">
-    <div className="container my-3"><a className="tdn" href="https://github.com/rootyss">Rooty</a></div>
-  </footer>
-);
+const Footer = () => {
+  const [cookies, setCookie] = useCookies(['cookieConsent']);
+
+  const handlerCookieConsent = () => {
+    setCookie('cookieConsent', true, { path: '/' });
+  };
+
+  return (
+    <footer className="border-top">
+      <div className="container my-3"><a className="tdn" href="https://github.com/rootyss">Rooty</a></div>
+      {
+        !cookies.cookieConsent
+          ? (
+            <CookieConsent handlerCookieConsent={handlerCookieConsent} />
+          )
+          : null
+      }
+    </footer>
+  );
+};
 
 export default Footer;
