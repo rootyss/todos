8ba1bed4c9057f8cc51b5e2633d06dc7df1930ae
rootyss <jaxent@yandex.ru>
2022-01-13 22:00:15 +0300

    rewrite I tasks

--- src/store/tasksSlice.js
+++ src/store/tasksSlice.js
@@ -11,10 +11,15 @@ const tasksSlice = createSlice({
   },
   reducers: {
     addTaskToState(state, action) {
-      const { tasks } = action.payload;
-      const keysTasksData = _.keys(tasks);
-      const newTasks = keysTasksData.map((key) => ({ ...tasks[key], id: key }));
-      return { ...state, tasks: newTasks };
+      const { tasks, uid } = action.payload;
+      const keysTasksData = _.entries(tasks);
+      const currentTasks = keysTasksData.reduce((acc, [key, task]) => {
+        if (task.addedToUid === uid || task.addedByUid === uid) {
+          acc.push({ ...task, id: key });
+        }
+        return acc;
+      }, []);
+      return { ...state, tasks: currentTasks };
     },
     setCurrentTabDetailsName(state, action) {
       const { name } = action.payload;
