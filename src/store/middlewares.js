311c8447668d6e07039eec836ffbc42d6017dea2
rootyss <jaxent@yandex.ru>
2022-01-13 18:21:06 +0300

    add default export

--- src/store/middlewares.js
+++ src/store/middlewares.js
@@ -1,7 +1,10 @@
-import { setUser, clearUser } from './userSlice.js';
+import { updateUser, updateUserData, clearUser } from './userSlice.js';
 
-export const localStorageMiddleware = () => (next) => (action) => {
-  if (setUser.match(action) && action.payload) {
+export default () => (next) => (action) => {
+  if (updateUser.fulfilled.match(action) && action.payload) {
+    localStorage.setItem('user', JSON.stringify(action.payload));
+  }
+  if (updateUserData.fulfilled.match(action) && action.payload) {
     localStorage.setItem('user', JSON.stringify(action.payload));
   }
   if (clearUser.match(action)) {
