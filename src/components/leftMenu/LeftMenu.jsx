d5c24bafa0e7de34ba55a3a2471524801be5d6d5
rootyss <jaxent@yandex.ru>
2022-01-14 18:14:57 +0300

    add outbox. delete upcoming

--- src/components/leftMenu/LeftMenu.jsx
+++ src/components/leftMenu/LeftMenu.jsx
@@ -3,8 +3,8 @@ import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
 import {
   INBOX_ROUTE,
+  OUTBOX_ROUTE,
   TODAY_ROUTE,
-  UPCOMING_ROUTE,
   NOTE_ROUTE,
   LABELS_ROUTE,
   ARCHIVE_ROUTE,
@@ -17,8 +17,8 @@ const LeftMenu = () => {
       <div className="left-menu-list">
         <ul className="left-menu-top">
           <li><Link className="left-menu-nav-link" to={INBOX_ROUTE}>{t('leftMenuTop.inbox')}</Link></li>
+          <li><Link className="left-menu-nav-link" to={OUTBOX_ROUTE}>{t('leftMenuTop.outbox')}</Link></li>
           <li><Link className="left-menu-nav-link" to={TODAY_ROUTE}>{t('leftMenuTop.today')}</Link></li>
-          <li><Link className="left-menu-nav-link" to={UPCOMING_ROUTE}>{t('leftMenuTop.upcoming')}</Link></li>
           <li><Link className="left-menu-nav-link" to={NOTE_ROUTE}>{t('leftMenuTop.notes')}</Link></li>
           <li><Link className="left-menu-nav-link" to={LABELS_ROUTE}>{t('leftMenuTop.labels')}</Link></li>
           <li><Link className="left-menu-nav-link" to={ARCHIVE_ROUTE}>{t('leftMenuTop.archive')}</Link></li>
